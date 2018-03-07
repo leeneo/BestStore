78cbeb6eba2253e65b7a79722df5cd5257a301c6
leeneo <leeneo@xingzhihen.com>
2018-03-07 09:18:47 +0800

    1

---
+++ BestStore.Web/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BestStore.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var str = "tesgt";
+            return View(str);
+        }
+    }
+}
