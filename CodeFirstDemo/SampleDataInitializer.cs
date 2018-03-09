3a8e785baf51855d28af9b3128aa05d26b0ecd08
leeneo <leeneo@xingzhihen.com>
2018-03-09 17:58:40 +0800

    1

--- CodeFirstDemo/SampleDataInitializer.cs
+++ CodeFirstDemo/SampleDataInitializer.cs
@@ -18,20 +18,23 @@ namespace CodeFirstDemo
         {
             // Add Blogs
             BlogRepository blogRepository = new BlogRepository(_dbContext);
-            int x=await blogRepository.AddAsync(new Blog { Url = "www.leeneo.cn" });
-            Console.WriteLine("ID为 "+x+" 的 blog 已添加");
-            
-            //_dbContext.Blogs.AddRange(new Blog[] {
-            //    new Blog{Url="http://xx.com/3"},
-            //    new Blog{Url="https://xx.com/4"}
-            //});
+            int x = await blogRepository.AddAsync(new Blog { Url = "www.leeneo.cn" });
+            Console.WriteLine("ID为 " + x + " 的 blog 已添加");
 
             // Add Posts
-            _dbContext.Posts.Add(new Post
-            {
-                BlogId = 2,
-                Content = "fwoiwerwr",
-                Title = "jjj"
+            _dbContext.Posts.AddRange(new Post[]{
+                new Post
+                {
+                    BlogId = 2,
+                    Content = "fwoiwerwr",
+                    Title = "jjj"
+                },
+                new Post
+                {
+                    BlogId = 1,
+                    Content = "fwoiwerwr",
+                    Title = "aaa"
+                }
             });
 
             // Submit data into Database
