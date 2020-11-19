50066e7c56ad770c74758f161bd3fe0ad3829bf6
302698648@qq.com <leeneo@xingzhihen.com>
2020-11-19 23:38:06 +0800

    1

--- DBFirstDemo/BloggingContext.cs
+++ DBFirstDemo/BloggingContext.cs
@@ -13,7 +13,6 @@ namespace DBFirstDemo
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
                 optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Blogging;Trusted_Connection=True;");
             }
         }
